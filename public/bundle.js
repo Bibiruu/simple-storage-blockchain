6f0e07c2bb4c75146734ce48d2927621561f2721
Maria <so.mariagloria@gmail.com>
2024-03-26 01:07:32 +0200

    setting up abi properly and initializing  contract from truffle console

--- public/bundle.js
+++ public/bundle.js
@@ -1,90 +1,101 @@
 const simpleStorageABI = [
-    [
+  [
+    {
+      "inputs": [],
+      "name": "data",
+      "outputs": [
         {
-          "inputs": [],
-          "name": "data",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "_data",
-              "type": "string"
-            }
-          ],
-          "name": "set",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "get",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
+          "internalType": "string",
+          "name": "",
+          "type": "string"
         }
       ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_data",
+          "type": "string"
+        }
+      ],
+      "name": "set",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ],
 ];
-
+//0xccdf5E879841501EfE62d351DB951072834be550
+//0x45408799B913127C86A73b8b6006331E171A0FD9
 const simpleStorageAddress = '0xccdf5E879841501EfE62d351DB951072834be550';
-const web3 = new Web3(window.ethereum);
-const simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
+const web3 = new Web3('HTTP://127.0.0.1:7545');
+const simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress, {
+  from: '0x45408799B913127C86A73b8b6006331E171A0FD9',
+  gasPrice: '20000000000'
+});
+console.log('SIMPLESTORAGE', simpleStorage)
 
 //bootstrap basics for snatching data
 document.addEventListener('DOMContentLoaded', async () => {
-    const $setData = document.getElementById('setData');
-    const $data = document.getElementById('data');
-    //truffle arrays of accounts
-    let accounts = [];
-    //populate accounts array
+  const $setData = document.getElementById('setData');
+  const $data = document.getElementById('data');
+  //truffle arrays of accounts
+  let accounts = [];
+  //populate accounts array
+  try {
+    accounts = await web3.eth.getAccounts();
+    console.log('success', accounts)
+  } catch (error) {
+    console.log('Error fetching accounts', error);
+  }
+
+  const getData = async () => {
     try {
-        accounts = web3.eth.getAccounts();
-        console.log('success', accounts)
+      //communicating with the smart contrct
+      const result = await simpleStorage.methods.get().call();
+        $data.innerHTML = result;
+      
     } catch (error) {
-        console.log('Error fetching accounts', error);
+      console.log('error getting data', error)
     }
-
-    const getData = async () => {
-        try {
-            //communicating with the smart contrct
-            const result = await simpleStorage.methods.get().call()
-            $data.innerHTML = result;
-        } catch (error) {
-            console.log('error getting data', error)
-        }
-    };
     getData();
+  };
+  
 
+  //console.log('DATA', getData())
 
-    $setData.addEventListener('submit', async (e) => {
-        e.preventDefault()
-        const data = e.target.elements[0].value;
-        try {
-            //ensure accunt populated
-            if (accounts.length === 0) {
-                // If accounts array is empty, fetch accounts
-                accounts = await web3.eth.getAccounts();
-            };
-            await simpleStorage.methods.set(data).send({ from: accounts[0] })
-            getData();
-        } catch (error) {
-            console.log('error in fetching data', error)
-        }
-    });
+  $setData.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const data = await e.target.elements[0].value;
+    try {
+      // If accounts array is empty, fetch accounts
+      if (accounts.length === 0) {
+        accounts = await web3.eth.getAccounts();
+      }
+      await simpleStorage.methods.set(data).send({ from: accounts[0] });
+      await getData();
+      
+      
+    } catch (error) {
+      console.log('error in fetching data', error)
+    }
+  });
 });
+
+
